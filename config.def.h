6780355b5ae606c8316429d2a41512b3d67aa4ab
Your Name <you@example.com>
2024-09-25 21:41:03 +0700

    change color that make me more cooler

--- config.def.h
+++ config.def.h
@@ -10,16 +10,17 @@ static const unsigned int gappx     = 10;       /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12", "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
-static const char col_gray1[]       = "#1a1b26"; // #111111
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#777c99"; // #eeeeee
-static const char col_gray4[]       = "#0f0f14"; // #333333
-static const char col_cyan[]        = "#7aa2f7"; // #1793d1
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+static const char *fonts[]          = { "monospace:size=12", "JoyPixels:pixelsize=15:antialias=true:autohint=true" };
+static const char normbordercolor[] = "#1b1d2b";
+static const char normbgcolor[]     = "#222436";
+static const char normfgcolor[]     = "#828bb8";
+static const char selbordercolor[]  = "#c8d3f5";
+static const char selbgcolor[]      = "#1b1d2b";
+static const char selfgcolor[]      = "#c8d3f5";
+static const char *colors[][3] = {
+       /*               fg           bg           border   */
+       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 
 /* tagging */
